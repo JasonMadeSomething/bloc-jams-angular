42ea5cdd9f4543d445e90e2b98e3a1e8b5d3e7d7
Jason Teitelman <jason@mailmarketingfla.com>
2016-03-09 15:57:06 -0500

    Assignment 10 Directives, Add volume control functionality

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -86,6 +86,12 @@
         SongPlayer.currentTime = null;
         
         /**
+        * @desc Current player volume
+        * @type {Number}
+        */
+        SongPlayer.volume = null;
+        
+        /**
         * @method play
         * @desc public method used to either play or unpause a song
         * @param {Object} song
@@ -154,6 +160,17 @@
         SongPlayer.setCurrentTime = function(time) {
             if (currentBuzzObject) {
                 currentBuzzObject.setTime(time);
+            }
+        };
+        
+        /**
+        * @function setVolume
+        * @desc Set the volume (out of 100) of the player
+        * @param {Number} volume
+        */
+        SongPlayer.setVolume = function(volume){
+            if (currentBuzzObject) {
+                currentBuzzObject.setVolume(volume);
             }
         };
         
